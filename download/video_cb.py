97f92471d1c11827b56fe201b1e1c0744d8f7229
Yaolong Huang <airekans@gmail.com>
2012-07-25 11:23:18 +0800

    Change the video_cb to include error conditions.

--- download/video_cb.py
+++ download/video_cb.py
@@ -5,7 +5,8 @@ from twisted.internet import reactor
 from twisted.internet.protocol import Protocol, ClientFactory, ServerFactory
 from twisted.protocols.basic import NetstringReceiver
 from twisted.internet.defer import Deferred, succeed, inlineCallbacks
-
+from shorturl_client import transformShortUrlAsync
+from download_client import downloadVideoFromUrlAsync
 
 class DownloadVideoServerProtocol(NetstringReceiver):
     """ The protocol to download a video from
@@ -23,87 +24,25 @@ class DownloadVideoServerProtocol(NetstringReceiver):
         def downloadVideoFromUrl(url):
             print "long url:", url
             d = downloadVideoFromUrlAsync(url)
-            d.addCallback(storeVideo)
-        
-        d.addCallback(downloadVideoFromUrl)
+
+            def errDownloadVideoFromUrl(err):
+                print "exception:", err
+            
+            d.addCallbacks(storeVideo, errDownloadVideoFromUrl)
+
+        def errTransformShortUrl(err):
+            print "exception:", err
+            
+        d.addCallbacks(downloadVideoFromUrl, errTransformShortUrl)
 
 class DownloadVideoServerFactory(ServerFactory):
     
     protocol = DownloadVideoServerProtocol
         
 
-class ShortUrlClientProtocol(NetstringReceiver):
-    """ The protocol to transform a short URL
-    to its original form.
-    """
-
-    def connectionMade(self):
-        self.sendRequest(self.factory.shortUrl)
-
-    def sendRequest(self, shortUrl):
-        self.sendString(shortUrl)
-
-    def stringReceived(self, url):
-        self.transport.loseConnection()
-        self.urlReceived(url)
-
-    def urlReceived(self, url):
-        print "recv url:", url
-        self.factory.handleUrl(url)
-
-
-class ShortUrlClientFactory(ClientFactory):
-
-    protocol = ShortUrlClientProtocol
-
-    def __init__(self, shortUrl, deferred):
-        self.shortUrl = shortUrl
-        self.deferred = deferred
-
-    def handleUrl(self, url):
-        d, self.deferred = self.deferred, None
-        d.callback(url)
-
-    def clientConnectionLost(self, _, reason):
-        if self.deferred is not None:
-            d, self.deferred = self.deferred, None
-            d.errback(reason)
-        #reactor.stop()
-
-    clientConnectionFailed = clientConnectionLost
-
-
-def getShortUrl():
-    try:
-        return raw_input('url: ')
-    except EOFError:
-        return None
-
-SHORTURL_HOST = "localhost"
-SHORTURL_PORT = 63333
-    
-def transformShortUrlAsync(shortUrl):
-    d = defer.Deferred()
-    reactor.connectTCP(SHORTURL_HOST, SHORTURL_PORT,
-                       ShortUrlClientFactory(shortUrl, d))
-    return d
-
-def downloadVideoFromUrlAsync(url):
-    # similate the latency to download the whole video files
-    d = defer.Deferred()
-    reactor.callLater(2, lambda: d.callback("video file"))
-    return d
-
 def storeVideo(video):
     print "store", video
 
-def startDownloadService():
-    while True:
-        shortUrl = getShortUrl()
-        if shortUrl is None:
-            break
-
-        downloadVideoFromShortUrlAsync(shortUrl)
 
 if __name__ == '__main__':
     port = reactor.listenTCP(62222, DownloadVideoServerFactory())
